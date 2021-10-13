d0ba8889acf5c616d92b03bd052988ca82336fed
cabraja <cabrajamihajlo@gmail.com>
2021-10-13 19:54:35 +0200

    added movie animations

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const SingleMovie = ({title,poster_path,vote_average}) => {
+const SingleMovie = ({title,poster_path,vote_average,overview}) => {
     return (
         <article>
-            <div className="movie-img" style={poster_path ? {backgroundImage:`url(https://image.tmdb.org/t/p/w500${poster_path})`} : {backgroundColor:'gainsboro'}} ></div>
+            <div className="movie-img" style={poster_path ? {backgroundImage:`url(https://image.tmdb.org/t/p/w500${poster_path})`} : {backgroundColor:'gainsboro'}} >
+                <div className="movie-desc">{overview}</div>
+            </div>
             <div className="movie-info">
                 <h2>{title}</h2>
                 <h3>{vote_average || "N/A"}	&#9733;</h3>
